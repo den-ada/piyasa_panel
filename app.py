3ae7b293dac9b9be3af1e22aaefa56fdd21a05fa
den-ada <101962456+den-ada@users.noreply.github.com>
2025-07-02 18:12:40 +0300

    app.py

--- app.py
+++ app.py
@@ -15,4 +15,5 @@ def index():
     return render_template("index.html", yorum=yorum)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
